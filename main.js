775989c7fd5835450ce757151a3aa5ce8519cb28
Manu Sporny <msporny@digitalbazaar.com>
2020-07-11 15:18:58 -0400

    Add support function framework for encoding.

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
+import {getContextUrls} from './lib/context';
+import {getCompressionDictionary, toCborld} from './lib/compression';
 
 /**
- * Encodes a given JSON-LD document into a CBOR-LD byte array.
- *
- * @param {object} [jsonldDocument] - The JSON-LD Document to convert to a
- *  CBOR-LD byte array.
- * @param {object} [options] - The options to use.
- *
- * @returns {Uint8Array} - A CBOR-LD encoded byte array.
- */
-export async function encode(jsonldDocument, options) {
-  return new Uint8Array(4);
+  * Encodes a given JSON-LD document into a CBOR-LD byte array.
+  *
+  * @param {object} [args] - The arguments to the function.
+  * @param {Array} [args.jsonldDocument] - The JSON-LD Document to convert to a
+  *  CBOR-LD byte array.
+  * @param {object} [args.options] - The options to use.
+  *
+  * @returns {object} - The compression dictionary.
+  */
+export async function encode({jsonldDocument, options}) {
+  const contextUrls = await getContextUrls({jsonldDocument});
+  const compressionDictionary = await getCompressionDictionary({contextUrls});
+  const cborldBytes = await toCborld({jsonldDocument, compressionDictionary});
+
+  return cborldBytes;
 }
 
 /**
  * Decodes a CBOR-LD byte array into a JSON-LD document.
  *
- * @param {Uint8Array} cborldBytes - The encoded CBOR-LD byte array to decode.
- * @param {object} options - The options to use.
+ * @param {object} [args] - The arguments to the function.
+ * @param {Uint8Array} [args.cborldBytes] - The encoded CBOR-LD byte array to
+ *   decode.
+ * @param {object} [args.options] - The options to use.
  *
  * @returns {Uint8Array} - Decoded array of id bytes.
  */
-export function decodeId(options) {
+export function decode({cborldBytes, options}) {
   return {};
 }
