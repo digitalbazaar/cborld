f21521850685f9acb1f4b98b0651995e3d210e01
Manu Sporny <msporny@digitalbazaar.com>
2020-07-18 18:29:04 -0400

    Implement decoding of uncompressed CBOR-LD.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {getContextUrls} from './lib/context';
-import {getTermEncodingMap, toCborld} from './lib/compression';
-export {getTermCodecs} from './lib/compression';
+import {getJsonldContextUrls} from './lib/context';
+export {getTermCodecs} from './lib/codec';
+import {getTermEncodingMap, toCborld} from './lib/encode';
+import {fromCborld} from './lib/decode';
 
 /**
   * Encodes a given JSON-LD document into a CBOR-LD byte array.
@@ -25,23 +26,10 @@ export {getTermCodecs} from './lib/compression';
   */
 export async function encode({
   jsonldDocument, appContextMap, appTermMap, diagnose, documentLoader}) {
-  let termEncodingMap = undefined;
-
-  // get the term encoding map by processing the JSON-LD Contexts
-  try {
-    const contextUrls = getContextUrls({jsonldDocument});
-    termEncodingMap =
-      await getTermEncodingMap({contextUrls, appTermMap, documentLoader});
-  } catch(e) {
-    // quietly ignore embedded context errors, generate uncompressed CBOR-LD
-    if(e.value !== 'ERR_EMBEDDED_JSONLD_CONTEXT_DETECTED') {
-      throw e;
-    }
-  }
 
   const cborldBytes =
     await toCborld({
-      jsonldDocument, appContextMap, appTermMap, termEncodingMap, diagnose});
+      jsonldDocument, appContextMap, appTermMap, documentLoader, diagnose});
 
   return cborldBytes;
 }
@@ -52,12 +40,26 @@ export async function encode({
  * @param {object} [args] - The arguments to the function.
  * @param {Uint8Array} [args.cborldBytes] - The encoded CBOR-LD byte array to
  *   decode.
- * @param {object} [args.options] - The options to use.
+ * @param {object} [args.appContextMap] - A map of JSON-LD Context URLs and
+ *   their associated CBOR-LD compression values (must be values greater than
+ *   32767 (0x7FFF)).
+ * @param {object} [args.appTermMap] - A map of JSON-LD terms and
+ *   their associated CBOR-LD compression codecs.
+ * @param {Function} [args.diagnose] - A function that, if provided, is called
+ *   with diagnostic information.
+ * @param {Function} [args.documentLoader] -The document loader to use when
+ *   resolving JSON-LD Context URLs. Takes a single argument, a document URL.
  *
  * @returns {Uint8Array} - Decoded array of id bytes.
  */
-export function decode({
+export async function decode({
   cborldBytes, appContextMap, appTermMap, diagnose, documentLoader}) {
+  let codecMap = undefined;
 
-  return {};
+  const jsonldDocument =
+    await fromCborld({
+      cborldBytes, appContextMap, appTermMap, codecMap, documentLoader,
+      diagnose});
+
+  return jsonldDocument;
 }
