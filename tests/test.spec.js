499759e836cb573993aa77bea7854278b230cbcf
David I. Lehn <dlehn@digitalbazaar.com>
2020-07-23 20:18:27 -0400

    Fix decoding of empty object.
    - Enable test to decode empty object.
    - Map.entries doesn't take argument.
    - Remove instanceof Map check since API requires a Map.
    - Normalize data to a Buffer since 'cbor' library requires one.
    - Normalize `cbor.decode` result value to a Map. It returns just an
      object when decoding an empty object. Simplifies later Map vs object
      usage.

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -12,24 +12,26 @@ global.should = chai.should();
 
 import {
   encode,
-  decode
+  decode,
+  documentLoader
 } from '..';
 
 describe('cborld', () => {
   describe('encode', () => {
     it('should encode an empty JSON-LD Document', async () => {
       const jsonldDocument = {};
-      const cborldBytes = await encode({jsonldDocument});
-      expect(cborldBytes).to.equalBytes('d90501a0');
+      const cborldBytes = await encode({jsonldDocument, documentLoader});
+      expect(cborldBytes).instanceof(Uint8Array);
+      expect(cborldBytes).equalBytes('d90501a0');
     });
     it.skip('should encode a simple JSON-LD Document', async () => {
     });
   });
   describe('decode', () => {
-    it.skip('should decode empty document CBOR-LD bytes', async () => {
+    it('should decode empty document CBOR-LD bytes', async () => {
       const cborldBytes = new Uint8Array([0xd9, 0x05, 0x01, 0xa0]);
-      const jsonldDocument = await decode({cborldBytes});
-      expect(jsonldDocument).to.deepEqual({});
+      const jsonldDocument = await decode({cborldBytes, documentLoader});
+      expect(jsonldDocument).deep.equal({});
     });
     it.skip('should decode simple CBOR-LD bytes', async () => {
     });
