18d26d803d27c6cb20a5c6bc7385c5ad13b43d28
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-10-14 13:32:26 -0400

    Add a failing test to guide 2.0 fixes.

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,6 @@
 /*!
 * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
 */
-
 import {
   default as chai,
   expect
@@ -27,6 +26,7 @@ describe('cborld', () => {
     it.skip('should encode a simple JSON-LD Document', async () => {
     });
   });
+
   describe('decode', () => {
     it('should decode empty document CBOR-LD bytes', async () => {
       const cborldBytes = new Uint8Array([0xd9, 0x05, 0x01, 0xa0]);
@@ -34,6 +34,149 @@ describe('cborld', () => {
       expect(jsonldDocument).deep.equal({});
     });
     it.skip('should decode simple CBOR-LD bytes', async () => {
+    });
+
+    it('should decode a CIT type token', async () => {
+      const cborldBytes = [
+        217, 5, 1, 164, 1, 25, 128, 0, 21, 4, 15, 75,
+        122, 217, 5, 1, 162, 1, 25, 135, 67, 4, 21, 17,
+        88, 59, 122, 0, 0, 218, 254, 86, 110, 99, 95, 229,
+        103, 219, 131, 235, 12, 30, 200, 156, 4, 245, 17, 239,
+        118, 94, 144, 171, 32, 2, 125, 159, 180, 66, 225, 77,
+        98, 179, 185, 245, 142, 170, 210, 219, 16, 27, 49, 87,
+        164, 22, 249, 197, 111, 77, 40, 23, 250, 147, 45, 164,
+        171
+      ];
+
+      const CONTEXT_URL = 'https://w3id.org/cit/v1';
+      /* eslint-disable max-len */
+      const CONTEXT = {
+        '@context': {
+          '@protected': 'true',
+          type: '@type',
+          ConcealedIdTokenCredential: 'https://w3id.org/cit#ConcealedIdTokenCredential',
+          concealedIdToken: {
+            '@id': 'https://w3id.org/cit#concealedIdToken',
+            '@type': '@id'
+          },
+          ConcealedIdToken: {
+            '@id': 'https://w3id.org/cit#ConcealedIdToken',
+            '@context': {
+              '@protected': true,
+              meta: {'@id': 'https://w3id.org/cit#meta', '@type': 'https://w3id.org/security#multibase'},
+              payload: {'@id': 'https://w3id.org/cit#payload', '@type': 'https://w3id.org/security#multibase'}
+            }
+          },
+          Ed25519Signature2020: {
+            '@id': 'https://w3id.org/security#Ed25519Signature2020',
+            '@context': {
+              '@protected': true,
+
+              id: '@id',
+              type: '@type',
+
+              sec: 'https://w3id.org/security#',
+              xsd: 'http://www.w3.org/2001/XMLSchema#',
+
+              challenge: 'sec:challenge',
+              created: {'@id': 'http://purl.org/dc/terms/created', '@type': 'xsd:dateTime'},
+              domain: 'sec:domain',
+              expires: {'@id': 'sec:expiration', '@type': 'xsd:dateTime'},
+              nonce: 'sec:nonce',
+              proofPurpose: {
+                '@id': 'sec:proofPurpose',
+                '@type': '@vocab',
+                '@context': {
+                  '@protected': true,
+
+                  id: '@id',
+                  type: '@type',
+
+                  sec: 'https://w3id.org/security#',
+
+                  assertionMethod: {'@id': 'sec:assertionMethod', '@type': '@id', '@container': '@set'},
+                  authentication: {'@id': 'sec:authenticationMethod', '@type': '@id', '@container': '@set'}
+                }
+              },
+              proofValue: {
+                '@id': 'https://w3id.org/security#proofValue',
+                '@type': 'https://w3id.org/security#multibase'
+              },
+              verificationMethod: {'@id': 'sec:verificationMethod', '@type': '@id'}
+            }
+          }
+        }
+      };
+
+      const appContextMap = new Map();
+      appContextMap.set('https://w3id.org/cit/v1', 0x8000);
+
+      const documentLoader = url => {
+        if(url === CONTEXT_URL) {
+          return {
+            contextUrl: null,
+            document: CONTEXT,
+            documentUrl: url
+          };
+        }
+        throw new Error('Invalid context url, cannot load!');
+      };
+
+      const decodedDocument = await decode({
+        appContextMap,
+        cborldBytes,
+        documentLoader
+      });
+
+      console.log(decodedDocument);
+    });
+
+    it('should round trip sample Age context', async () => {
+      const AGE_CONTEXT_URL = 'https://w3id.org/age/v1';
+      const AGE_CONTEXT = {
+        '@context': {
+          '@protected': 'true',
+          id: '@id',
+          type: '@type',
+          ageOver: {
+            '@id': 'https://w3id.org/age#ageOver',
+            '@type': 'http://www.w3.org/2001/XMLSchema#positiveInteger'
+          }
+        }
+      };
+      const jsonldDocument = {
+        '@context': 'https://w3id.org/age/v1',
+        ageOver: 21
+      };
+      const appContextMap = new Map();
+      appContextMap.set('https://w3id.org/age/v1', 0x8743);
+
+      const documentLoader = url => {
+        if(url === AGE_CONTEXT_URL) {
+          return {
+            contextUrl: null,
+            document: AGE_CONTEXT,
+            documentUrl: url
+          };
+        }
+        throw new Error('Invalid context url, cannot load!');
+      };
+
+      const encodedBytes = await encode({
+        jsonldDocument,
+        appContextMap,
+        documentLoader
+      });
+
+      const decodedDocument = await decode({
+        appContextMap,
+        cborldBytes: encodedBytes,
+        documentLoader
+      });
+
+      expect(decodedDocument).to.eql({
+        '@context': 'https://w3id.org/age/v1', ageOver: 21
+      });
     });
   });
 });
