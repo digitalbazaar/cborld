763d267a22872fe1e6b2d12dfc91fe6f839e6e2c
Dave Longley <dlongley@digitalbazaar.com>
2021-04-10 19:54:40 -0400

    Remove old loader code/move to CLI.

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -9,17 +9,13 @@ import {default as chaiBytes} from 'chai-bytes';
 chai.use(chaiBytes);
 global.should = chai.should();
 
-import {
-  encode,
-  decode,
-  documentLoader
-} from '..';
+import {encode, decode} from '..';
 
 describe('cborld', () => {
   describe('encode', () => {
     it('should encode an empty JSON-LD Document', async () => {
       const jsonldDocument = {};
-      const cborldBytes = await encode({jsonldDocument, documentLoader});
+      const cborldBytes = await encode({jsonldDocument});
       expect(cborldBytes).instanceof(Uint8Array);
       expect(cborldBytes).equalBytes('d90501a0');
     });
@@ -101,7 +97,7 @@ describe('cborld', () => {
   describe('decode', () => {
     it('should decode empty document CBOR-LD bytes', async () => {
       const cborldBytes = new Uint8Array([0xd9, 0x05, 0x01, 0xa0]);
-      const jsonldDocument = await decode({cborldBytes, documentLoader});
+      const jsonldDocument = await decode({cborldBytes});
       expect(jsonldDocument).deep.equal({});
     });
 
